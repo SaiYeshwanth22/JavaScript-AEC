9344baacad7dc67f45bd514690145f3fa1e1b679
Sai Yeshwanth <its.saiyeshwanth@gmail.com>
2026-02-23 19:54:31 +0530

    Added Question 4

---
+++ Question4.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
+}
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log("Even:", i);
+    }
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`5 x ${i} = ${5 * i}`);
+}

bc5b352817bf83565459313a87fdd89f642a94b4
Sai Yeshwanth <its.saiyeshwanth@gmail.com>
2026-02-23 19:56:30 +0530

    Added Question 6

---
+++ Question6.js
@@ -0,0 +1,13 @@
+let user = {
+    name: "Sai",
+    email: "sai@example.com",
+    password: "12345",
+    age: 21
+};
+
+console.log(user.name);
+
+user.email = "sai.new@example.com";
+user.isAdmin = true;
+
+console.log(user);

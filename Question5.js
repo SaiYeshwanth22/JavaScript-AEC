f8b41cea3dc617410a0de6e895c462f220cbcfe2
Sai Yeshwanth <its.saiyeshwanth@gmail.com>
2026-02-23 19:55:11 +0530

    Added Question 5

---
+++ Question5.js
@@ -0,0 +1,8 @@
+let students = ["Rahul", "Ananya", "Kiran", "Meera", "Vikram"];
+
+students.push("Arjun");
+students.pop();
+
+for (let i = 0; i < students.length; i++) {
+    console.log(students[i]);
+}
